bc1bbc439f0a05c110f57d4bf46e602633eff5b1
Steffi Wehrle <steffinchen@gmail.com>
2020-02-02 14:33:28 +0100

    :fire: remove unused things

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/favicon.png',
-  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   firstName: 'Stefanie',
   lastName: 'Wehrle',
   // social
